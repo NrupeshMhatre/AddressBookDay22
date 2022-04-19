6653bcf5220a61ddd4deacf7b1f8d204a9612998
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-04-19 15:55:40 +0530

    Contact Creating

--- src/main/java/addressbookday22/AdressBook.java
+++ src/main/java/addressbookday22/AdressBook.java
@@ -5,66 +5,65 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AdressBook {
-	public void Contact() { 
+	public void contact() {
 
-		static Scanner sc = new Scanner(System.in);
-	    static List<ContactInfo> list = new LinkedList<ContactInfo>();
+		ContactInfo contact = new ContactInfo();
+		Scanner sc = new Scanner(System.in);
 
-	
-	    public static void addingContact()
-	    {
-	        System.out.println("Enter your firstName : ");
-	        String firstName = sc.nextLine();
-	        System.out.println("Enter your lastName : ");
-	        String lastName = sc.nextLine();
-	        System.out.println("Enter your address : ");
-	        String address = sc.nextLine();
-	        System.out.println("Enter your city : ");
-	        String city = sc.nextLine();
-	        System.out.println("Enter your state : ");
-	        String state = sc.nextLine();
-	        System.out.println("Enter your pin : ");
-	        String pin = sc.nextLine();
-	        System.out.println("Enter your MobileNo : ");
-	        String MobileNo = sc.nextLine();
-	        System.out.println("Enter your email : ");
-	        String email = sc.nextLine();
-	        ContactInfo obj = new ContactInfo(firstName, lastName, city, state, pin, MobileNo, email);
-	        list.add(obj);
-	    }
+		System.out.print("\nEnter First Name  : ");
+		String firstName = sc.nextLine();
+		contact.setFirstName(firstName);
 
+		System.out.print("\nEnter Last Name  : ");
+		String lastName = sc.nextLine();
+		contact.setLastName(lastName);
 
-	    public static void editContact()
-	    {
-	        
-	        System.out.println("Enter first name for confirmation : ");
-	        String firstName = sc.nextLine();
-	        for (int i = 0; i < list.size(); i++) {
-	            if (list.get(i).getFirstName().equalsIgnoreCase(firstName))
-	            {
-	                list.remove(i);
-	                addingContact();
-	                System.out.println("Successfully Edit data");
-	            }
-	            else {
-	                System.out.println("No data found in Address Book");
-	            }
-	        }
-	    }
+		System.out.print("\nEnter E-mail  : ");
+		String email = sc.nextLine();
+		contact.setEmail(email);
 
+		System.out.print("\nEnter Address  : ");
+		String address = sc.nextLine();
+		contact.setAddress(address);
 
-	    public static void main(String[] args) {
+		System.out.print("\nEnter City  : ");
+		String city = sc.nextLine();
+		contact.setCity(city);
 
-	    	AdressBook Book = new AdressBook();
-	        
-	        System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
-	        
-	        System.out.println("Enter details of new contact");
-	        addingContact();
-	        
-	        editContact();
+		System.out.print("\nEnter State  : ");
+		String state = sc.nextLine();
+		contact.setState(state);
 
-	        System.out.println(list); 
-	    }
+		System.out.print("\nEnter Phone Number  : ");
+		long phoneNo = sc.nextLong();
+		contact.setPhoneNo(phoneNo);
+
+		System.out.print("\nEnter Zip  : ");
+		int zip = sc.nextInt();
+		contact.setZip(zip);
+		showContact(contact);
+		
+
 	}
+
+	public void showContact(ContactInfo contact) {
+
+		System.out.print("\n-----------------");
+		System.out.print("\nFirst Name  : " + contact.getFirstName());
+		System.out.print("\nLast Name   : " + contact.getLastName());
+		System.out.print("\nAddress     : " + contact.getAddress());
+		System.out.print("\nCity        : " + contact.getCity());
+		System.out.print("\nState       : " + contact.getState());
+		System.out.print("\nPhone Number : " + contact.getPhoneNo());
+		System.out.print("\nE-mail      : " + contact.getEmail());
+		System.out.print("\nZip         : " + contact.getZip());
+
+	}
+
+	public static void main(String[] args) {
+
+		AdressBook book = new AdressBook ();
+		book.contact();
+	}
+}
 
