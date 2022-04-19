05c097550611edc378af15937bab104d0368be49
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-04-19 19:13:30 +0530

    NO Duplicate Entry

--- src/main/java/addressbookday22/AdressBook.java
+++ src/main/java/addressbookday22/AdressBook.java
@@ -6,50 +6,131 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AdressBook {
-	 static Scanner sc = new Scanner(System.in);
-	    static List<ContactInfo> list = new ArrayList<ContactInfo>();
+	private ArrayList<ContactInfo> contactBook = new ArrayList<ContactInfo>();
+	Scanner sc = new Scanner(System.in);
+	private static int numberOfConatcts = 0;
 
-	    
-	    public static void contactAdd()
-	    {
-	        System.out.println("Enter your firstName : ");
-	        String firstName = sc.nextLine();
-	        System.out.println("Enter your lastName : ");
-	        String lastName = sc.nextLine();
-	        System.out.println("Enter your address : ");
-	        String address = sc.nextLine();
-	        System.out.println("Enter your city : ");
-	        String city = sc.nextLine();
-	        System.out.println("Enter your state : ");
-	        String state = sc.nextLine();
-	        System.out.println("Enter your zipCode : ");
-	        String zip = sc.nextLine();
-	        System.out.println("Enter your phoneNo : ");
-	        long phoneNo = sc.nextLong();
-	        System.out.println("Enter your emailId : ");
-	        String email = sc.nextLine();
-	        ContactInfo obj = new ContactInfo(firstName, lastName, address, city, state, zip, phoneNo, email);
-	        list.add(obj);
-	    }
-	    public static void addPersons() {
-	        System.out.println("Enter number of persons added to be in address book : ");
-	        int noOfPersons = sc.nextInt();
-	        int count  = 1;
-	        while (count <= noOfPersons) {
-	            contactAdd();
-	            count++;
-	        }
-	    }
+	public AdressBook() {
+		// TODO Auto-generated constructor stub
+	}
 
-	    public static void main(String[] args) {
+	public void contactAdd() {
+		System.out.println("Enter Person details:");
 
-	    	AdressBook ad = new AdressBook();
-	       
-	        System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
-	        
-	        addPersons();
-	        System.out.println(list); 
-	    }
-	 
+		ContactInfo person = details();
+		boolean isDuplicate = contactBook.stream().anyMatch(contact -> contact.equals(person));
+		if (isDuplicate) {
+			System.out.println("Duplicate data entry. discarded");
+		} else {
+			contactBook.add(person);
+		}
+	}
+
+	public void edit() {
+		System.out.println("enter the name to edit contact details");
+		String name = sc.next();
+		System.out.println("enter the choice to edit details");
+		System.out.println("1.First Name\\n2.Last Name\\n3.City\\n4.State\\n5.Zip Code\\n6.Phone\\n7.Email");
+		int choice = sc.nextInt();
+		int index = 0;
+		for (index = 0; index < numberOfConatcts; index++)
+			if (name.equals(contactBook.get(index).getFirstName())) {
+				System.out.println("name exists , now enter the new details");
+				switch (choice) {
+				case 1:
+					System.out.println("Enter new First Name");
+					String newFirstName = sc.next();
+					contactBook.get(index).setFirstName(newFirstName);
+					break;
+				case 2:
+					System.out.println("Enter new Last Name");
+					String newLastName = sc.next();
+					contactBook.get(index).setLastName(newLastName);
+					break;
+				case 3:
+					System.out.println("Enter new City");
+					String newCity = sc.next();
+					contactBook.get(index).setCity(newCity);
+					break;
+				case 4:
+					System.out.println("Enter new State");
+					String newState = sc.next();
+					contactBook.get(index).setState(newState);
+					break;
+				case 5:
+					System.out.println("Enter new State");
+					int newZip = sc.nextInt();
+					contactBook.get(index).setZip(newZip);
+					break;
+				case 6:
+					System.out.println("Enter new Phone Number");
+					int newPNumber = sc.nextInt();
+					contactBook.get(index).setPhoneNumber(newPNumber);
+					break;
+				case 7:
+					System.out.println("Enter new Email");
+					String newEmail = sc.next();
+					contactBook.get(index).setEmail(newEmail);
+					break;
+				}
+
+			} else {
+				System.out.println("No contact found");
+			}
+	}
+
+	public void delete() {
+		int index;
+		System.out.println("Enter the name of the contact to delete");
+		String name = sc.next();
+		for (index = 0; index < numberOfConatcts; index++)
+			if (contactBook.get(index).getFirstName().equals(name)) {
+				break;
+			}
+		while (!contactBook.get(index + 1).equals(null)) {
+			contactBook.set(index, contactBook.get(index + 1));
+			index++;
+		}
+		contactBook.set(index, null);
+		System.out.println("Deleted details of : " + name);
+	}
+
+	public void display() {
+		ContactInfo person;
+		System.out.println("Enter name to see details");
+		String name = sc.next();
+
+		for (int i = 0; i < contactBook.size(); i++) {
+			if (contactBook.get(i).getFirstName().equals(name)) {
+				person = contactBook.get(i);
+				System.out.println(person);
+			}
+		}
+	}
+
+	private static ContactInfo details() {
+		try (Scanner sc = new Scanner(System.in)) {
+			ContactInfo person1 = new ContactInfo();
+
+			System.out.println("Enter firstName:");
+			person1.setFirstName(sc.next());
+			System.out.println("Enter SecondName:");
+			person1.setLastName(sc.next());
+			System.out.println("Enter Address:");
+			person1.setAddress(sc.next());
+			System.out.println("Enter City:");
+			person1.setCity(sc.next());
+			System.out.println("Enter State:");
+			person1.setState(sc.next());
+			System.out.println("Enter Pin code:");
+			person1.setZip(sc.nextInt());
+			System.out.println("Enter Phone number:");
+			person1.setPhoneNumber(sc.nextInt());
+			System.out.println("Enter email:");
+			person1.setEmail(sc.next());
+			return person1;
+		}
+	}
+
 }
 
--- src/main/java/addressbookday22/ContactInfo.java
+++ src/main/java/addressbookday22/ContactInfo.java
@@ -2,82 +2,101 @@ package addressbookday22;
 
 public class ContactInfo {
 	private String firstName;
-    private String lastName;
-    private String address;
-    private String city;
-    private String state;
-    private String zip;
-    private long phno;
-    private String email;
-    
-    @Override
-    public String toString()
-    {
-        return "Contact [firstName: "+firstName+" , "+"lastName: "+lastName+" , "+"address: "+address+" , "+"city: "+city+" , "+
-                "state: "+state+" , "+"zip: "+zip+" , "+"phoneNo: "+phno+" , "+"email: "+email+ "]" ;
-    }
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String email;
+	private int zip, phoneNumber;
 
-    public ContactInfo(String firstName, String lastName, String address, String city, String state, String zip, long phoneNo, String email)
-    {
-        super();
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phno = phno;
-        this.email = email;
-    }
+	public ContactInfo() {
+	}
 
-    
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getState() {
-        return state;
-    }
-    public void setState(String state) {
-        this.state = state;
-    }
-    public String getZip() {
-        return zip;
-    }
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-    public long getPhoneNo() {
-        return phno;
-    }
-    public void setPhoneNo(long phno) {
-        this.phno = phno;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public ContactInfo(String firstName, String lastName, String address, String city, String state, String email,
+			int zip, int phoneNumber) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.email = email;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "ContactPerson [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
+				+ city + ", state=" + state + ", email=" + email + ", zip=" + zip + ", phoneNumber=" + phoneNumber
+				+ "]";
+	}
+
+	
+		
+	
 }
